af7fb719ecc3c9c8930c09492d57c982c097b7fe
Khadaassi <aassikhadija@yahoo.fr>
2025-02-18 16:23:33 +0100

    Firt commit in this branch

---
+++ App/models/user.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, Field
+
+
+class User(SQLModel, table=True):
+    id : int = Field(primary_key=True)
+    email : str
+    password : str
+    is_active : bool = False
+    role : str
+
+    def __str__(self):
+        return f"{self.email} - {self.role}"
