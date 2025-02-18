875adf31716c510d7b9e7c8ffc6b5cd0ff8b90df
Khadaassi <aassikhadija@yahoo.fr>
2025-02-18 16:23:33 +0100

    Firt commit in this branch

---
+++ App/core/database.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, create_engine, Session
+from App.core.config import settings
+
+# Database setup
+database_url = "sqlite:///./database.db"
+engine = create_engine(settings.SQLITE_URL, connect_args={"check_same_thread":False})
+SQLModel.metadata.create_all(engine)
+
+# Dependency
+def get_db():
+    with Session(engine) as session:
+        yield session
